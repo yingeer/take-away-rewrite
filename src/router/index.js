972b7834631ba5d4f21cdf4761f881e0881368ea
yingeer <2973780290@qq.com>
2019-10-15 09:10:57 +0800

    pages的组件添加到了router中

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Msite from '../pages/miste/Msite.vue'
+import Search from "../pages/search/Search.vue"
+import Order from "../pages/order/Order.vue"
+import Profile from "../pages/profile/Profile.vue"
+
 
 Vue.use(Router)
 
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: '/msite',
       name: "misite",
       component: Msite
+    },
+    {
+      path: "/order",
+      name: "order",
+      component: Order
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: Profile
+    },
+    {
+      path: "/search",
+      name: "search",
+      component: Search
     }
   ]
 })
