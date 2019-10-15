cdc0240f02c6e7b498d5da2443da7746d260ba75
yingeer <2973780290@qq.com>
2019-10-15 09:44:19 +0800

    大致完成App.vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+        <footer-guide></footer-guide>
+    </div>
+</template>
+
+<script>
+import FooterGuide from './components/footerGuid/FooterGuid.vue'
+
+export default {
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    background #fee
+</style>
